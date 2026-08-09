3b1fb73de8016e6b38168605456543394a288c54
sapthapreethi-2006 <senthil63svg@gmail.com>
2026-08-09 15:35:23 +0530

    Update API URLs to use production endpoints

--- invent-front/src/Services/TransactionService.jsx
+++ invent-front/src/Services/TransactionService.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const STOCK_URL = 'http://localhost:9191/invent/stock';
-const TRANS_URL = 'http://localhost:9191/invent/trans';
-const ANA_URL   = 'http://localhost:9191/invent/analysis';
+const STOCK_URL = 'https://smartshelfx-backend-vh6n.onrender.com/invent/stock';
+const TRANS_URL = 'https://smartshelfx-backend-vh6n.onrender.com/invent/trans';
+const ANA_URL = 'https://smartshelfx-backend-vh6n.onrender.com/invent/analysis';
 
 export const saveTransaction = (transaction) => {
   return axios.post(STOCK_URL, transaction, {
@@ -17,35 +17,37 @@ export const findTransactionById = (id) => {
 };
 
 export const showAllTransactions = () => {
-    return axios.get(STOCK_URL, {
-      withCredentials: true
-    });
-}
-export const  removeTransactionById = (id) => {
-    return axios.delete(`${STOCK_URL}/${id}`, {
-      withCredentials: true
-    });
-}
-export const transactionIdGenerate = () => {
-    return axios.get(`${TRANS_URL}`, {
-      withCredentials: true
-    });
-}
+  return axios.get(STOCK_URL, {
+    withCredentials: true
+  });
+};
 
-export const findTransactionsByType= (type) => {
-    return axios.get(`${TRANS_URL}/${type}`, {
-      withCredentials: true
-    });
-}
+export const removeTransactionById = (id) => {
+  return axios.delete(`${STOCK_URL}/${id}`, {
+    withCredentials: true
+  });
+};
+
+export const transactionIdGenerate = () => {
+  return axios.get(`${TRANS_URL}`, {
+    withCredentials: true
+  });
+};
+
+export const findTransactionsByType = (type) => {
+  return axios.get(`${TRANS_URL}/${type}`, {
+    withCredentials: true
+  });
+};
 
 export const getDemandByProduct = (id) => {
-    return axios.get(`${ANA_URL}/${id}`, {
-        withCredentials: true
-    })
-}
+  return axios.get(`${ANA_URL}/${id}`, {
+    withCredentials: true
+  });
+};
 
 export const getProductWiseTotalSale = () => {
-    return axios.get(ANA_URL, {
-      withCredentials: true
-});
-}
+  return axios.get(ANA_URL, {
+    withCredentials: true
+  });
+};
